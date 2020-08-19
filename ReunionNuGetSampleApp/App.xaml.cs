699a1085c2b0cab6d8c7ec293387069bb250fbea
Andrew Leader <aleader@microsoft.com>
2020-08-18 20:02:33 -0700

    Close to working

---
+++ ReunionNuGetSampleApp/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Reunion;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Windows.UI.Notifications;
+using Microsoft.UI.Notifications;
+
+namespace ReunionNuGetSampleApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ReunionApp.OnActivated += ReunionApp_OnActivated;
+
+            ToastNotifier notifier;
+        }
+
+        private void ReunionApp_OnActivated(Windows.ApplicationModel.Activation.IActivatedEventArgs args)
+        {
+            Dispatcher.Invoke(delegate
+            {
+                MessageBox.Show((args as ToastNotificationActivatedEventArgsCompat).Argument);
+            });
+        }
+    }
+}
